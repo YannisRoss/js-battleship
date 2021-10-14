215bd0edfe90abbaa9143e117335c593ab3a62fb
grimhamr <rossiadisj@gmail.com>
2021-10-14 17:27:12 +0300

    build gameboard generation, refactor ships to have positions

--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -1,8 +1,18 @@
 
 
-let  gameboardFactory = () => {
+let gameboardFactory = () => {
+    let a = []
+    let b = []
+    let tiles = [a,b]
+
+    let i = 0
+    while (i < 50) {
+        a.push(i)
+        b.push(i)
+        i++
+    }
     
-    tiles = []
+
 
 
 }
--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,22 +1,22 @@
 
 
-let shipFactory = (size) => {
+let shipFactory = (size, positions) => {
  
-
-    let positions = []
+    let positions = positions
+    let segments = []
     let i = 0;
 
       while (i < size) {
-        positions.push('intact')
+        segments.push(positions[i]+'intact')
         i++
       }
     let isHit = false;
     let isSunk = false;
 
     function hit(position){
-        positions[position] = 'hit';
+        segments[position] = 'hit';
         this.isHit = true
-        if (positions.every(e => e == 'hit')){
+        if (segments.every(e => e == 'hit')){
             this.isSunk = true;
         }
     }
